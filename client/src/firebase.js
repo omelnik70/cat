ebf7947c673ac4448fc3c3e8d09fc792f1b23fc1
Serhii <98398176+omelnik70@users.noreply.github.com>
2022-11-07 14:58:12 +0200

    "Add .env.local"

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -8,13 +8,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBuk71HV6bulm8ec-63C4osAONHRLMMhR0",
-  authDomain: "alihelper-8a62d.firebaseapp.com",
-  projectId: "alihelper-8a62d",
-  storageBucket: "alihelper-8a62d.appspot.com",
-  messagingSenderId: "60154718509",
-  appId: "1:60154718509:web:a8e716f3cbf2adf2b00ba4",
-  measurementId: "G-3FQKHGGCFV"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
